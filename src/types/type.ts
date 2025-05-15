7e250a7849e1f870dc28bab109dd1cf919fa39e4
SeanChen <angus.chen9001@gmail.com>
2025-05-15 21:35:02 +0800

    導入 3d room，調整燈光，引入類似下雪的動畫

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,3 +1,6 @@
+import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from 'three';
+
 export type Word = {
   text: string;
   imgPath: string;
@@ -7,4 +10,14 @@ export type ButtonProps = {
   className?: string
   id?: string
   text: string
+}
+
+export type GLTFResult = GLTF & {
+  nodes: {
+    [key: string]: THREE.Mesh;
+  }
+  materials: {
+    [key: string]: THREE.MeshStandardMaterial | THREE.MeshPhongMaterial | THREE.MeshLambertMaterial;
+  }
+  scene: THREE.Scene;
 }
