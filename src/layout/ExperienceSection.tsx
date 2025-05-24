c3691612b49a4f28e8411ab9225cc5b14d021404
SeanChen <angus.chen9001@gmail.com>
2025-05-24 11:53:48 +0800

    - 更改 timeline 背景色 - 更換我自己的 experience 的 icon 和 desc - 為 experience layout 的部分加上 gsap 動畫效果

--- src/layout/ExperienceSection.tsx
+++ src/layout/ExperienceSection.tsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants'
-import GlowCard from '../components/GlowCard';
+import GlowCard from '../components/GlowCard'
+import gsap  from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const ExperienceSection: React.FC = () => {
+  useGSAP(() => {
+    gsap.utils.toArray<HTMLElement>(".timeline-card").forEach((card) => {
+      gsap.from(card, {
+        xPercent: -100,
+        opacity: 0,
+        transformOrigin: "left left",
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+        }
+      })
+    })
+
+    gsap.to(".timeline", {
+      transformOrigin: "bottom bottom",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".timeline",
+        start: "top center",
+        end: "70% center",
+        onUpdate: (self) => {
+          gsap.to(".timeline", {
+            scaleY: 1 - self.progress,
+          })
+        }
+      },
+    })
+
+    gsap.utils.toArray<HTMLElement>(".expText").forEach((text) => {
+      gsap.from(text, {
+        xPercent: 0,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: text,
+          start: "top 60%",
+        }
+      })
+    })
+  }, [])
+
   return (
     <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl:px-0">
       <div className="w-full h-full md:px-20 px-5">
@@ -30,14 +79,14 @@ const ExperienceSection: React.FC = () => {
                         <div className="timeline" />
                         <div className="gradient-line w-1 h-full" />
                       </div>
-                      <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-30">
+                      <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
                         <div className="timeline-logo">
                           <img src={card.logoPath} alt={card.logoPath} />
                         </div>
                         <div>
-                          <h1 className="font-semibold text-3xl">{card.title}</h1>
-                          <p className="my-5 text-white-50">🗓️ {card.date}</p>
-                          <p className="text-[#839cb5] italic">Responsibilities</p>
+                          <h1 className="font-semibold text-3xl text-pink-400">{card.title}</h1>
+                          <p className="my-5 text-yellow-300">🗓️ {card.date}</p>
+                          <p className="text-cyan-300 italic">Responsibilities</p>
                           <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
                             {card.responsibilities.map((responsibility) => (
                               <li className="text-lg" key={responsibility}>
