1a3490d8cd41f5157fc514b361535c02e169298b
SeanChen <angus.chen9001@gmail.com>
2025-06-06 03:12:42 +0800

    封裝 star component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { navLinks } from '@/constants/index'
 import GradientText from './GradientText'
 
+const Star = () => (
+  <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
+    version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
+    <defs></defs>
+    <g id="Layer_x0020_1">
+      <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+      <path
+        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+        className="fil0"></path>
+    </g>
+  </svg>
+)
+
 const NavBar: React.FC = () => {
   const [scrolled, setScrolled] = useState(false)
   const [activeSection, setActiveSection] = useState<string | null>(null)
@@ -179,90 +192,7 @@ const NavBar: React.FC = () => {
         <a href="#contact" className="contact-btn group relative inline-block">
           <div className="inner glow-cyan">
             <span>Contact Me</span>
-            <div className="star-1">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
-            <div className="star-2">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
-            <div className="star-3">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
-            <div className="star-4">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
-            <div className="star-5">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
-            <div className="star-6">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
-            <div className="star-7">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53"
-                version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                <defs></defs>
-                <g id="Layer_x0020_1">
-                  <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                  <path
-                    d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                    className="fil0"></path>
-                </g>
-              </svg>
-            </div>
+            {Array.from({ length: 7 }, (_, i) => <div key={i} className={`star-${i + 1}`}><Star /></div>)}
           </div>
         </a>
       </div>
